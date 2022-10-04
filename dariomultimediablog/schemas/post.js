dd83611aa5f38e478e8b9ed94fe2f4fc31a51bb6
DarioCostaAlves <dario.c.alves22@gmail.com>
2022-10-04 23:59:09 +0100

    fetching data from sanity

--- dariomultimediablog/schemas/post.js
+++ dariomultimediablog/schemas/post.js
@@ -6,6 +6,12 @@ export default {
     {
       name: 'title',
       title: 'Title',
+      description: 'Keep titles short and sweet',
+      type: 'string',
+    },
+    {
+      name: 'description',
+      title: 'Description',
       type: 'string',
     },
     {
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,16 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Header from '../components/Header';
 import Banner from '../components/Banner';
+import { sanityClient, urlFor } from '../sanity';
+import {Post} from '../typings';
+import Link from 'next/link';
 
-const Home: NextPage = () => {
+interface Props {
+  posts: [Post];
+}
+
+export default function Home({posts}: Props) {
+  console.log(posts);
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
@@ -13,9 +21,46 @@ const Home: NextPage = () => {
 
       <Header />
       <Banner />
+
+      {/* Posts */}
+      <div>
+        {posts.map(post => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div>
+              {post.mainImage && (
+                <img 
+                  src={urlFor(post.mainImage).url()!} 
+                  alt="Post image"
+                />
+              )}              
+            </div>
+            
+          </Link>
+        ))}
+      </div>
       
     </div>
   );
 }
 
-export default Home
+export const getServerSideProps = async () => {
+  const query = `*[_type == "post"]{
+    _id,
+    title,
+    author -> {
+      name,
+      image,
+  },
+    description,
+    mainImage,
+    slug
+  }`;
+
+  const posts = await sanityClient.fetch(query);
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
