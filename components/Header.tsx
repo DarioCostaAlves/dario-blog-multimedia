97ea9439c03b5d6552611146117b14610498f03e
DarioCostaAlves <dario.c.alves22@gmail.com>
2022-10-08 16:26:59 +0100

    adding real info

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,17 +13,14 @@ function Header() {
                 />
             </Link>
             <div className="hidden md:inline-flex
-            items-center space-x-5">
-                <h3>About</h3>
-                <h3>Contact</h3>
+            items-center space-x-5">                
                 <h3 className="text-white bg-green-600
-                px-4 py-1 rounded-full">Follow</h3>
+                px-4 py-1 rounded-full">Posts</h3>
             </div>           
         </div>
-        <div className="flex items-center space-x-5 text-green-600">
-            <h3>Sign in</h3>
+        <div className="flex items-center space-x-5 text-green-600">            
             <h3 className="border px-4 py-1 rounded-full
-            border-green-600">Get Started</h3>
+            border-green-600">Blog</h3>
         </div>
     </header>
   )
